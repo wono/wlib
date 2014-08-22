5acf356ee9e45da988fed020321423a4705d9eab
wono <wono@live.com>
2014-08-22 16:18:50 -0700

    Update wstring_copy.h

--- wstring_copy.h
+++ wstring_copy.h
@@ -1,15 +1,16 @@
-/*  WSTRING_COPY                            *
- *                                          *
- *      last modified:  2014/05/08          *
- *      first wrote:    2014/05/01          *
- *                                          *
- *      wono (a.k.a wonho)                  */
+/*  WSTRING_COPY
+ *
+ *      last modified:  2014/08/22
+ *      first wrote:    2014/05/01
+ *
+ *      Wonho Lim
+ */
 
 #ifndef _wstring_copy_
 #define _wstring_copy_
 
 #include <stdlib.h>
-#include <wstring/wstring_length.h>
+#include "wstring_length.h"
 
 /*  result string should be dynamically allocated within memory *
  *      - i.e., assumed to be used calloc, malloc, or realloc.  *
